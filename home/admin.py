482c5a138d63ad011100805cf1867bdeb00cf2ca
JR <john.w.ritter@outlook.com>
2023-01-31 17:49:12 -0800

    Created DB tables

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Customer
+from .models import Items
+from .models import ItemSupplier
+from .models import Employee
+from .models import Transaction
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Items)
+admin.site.register(ItemSupplier)
+admin.site.register(Employee)
+admin.site.register(Transaction)
+
+class Customer(admin.ModelAdmin):
+    exclude = ('CardNum')
--- home/models.py
+++ home/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+
+#Employee Table
+class Employee(models.Model):
+    EmployeeFirst = models.CharField(max_length=30)
+    EmployeeLast = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.EmployeeLast
+
+
+#customer table
+class Customer(models.Model):
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=30)
+    City = models.CharField(max_length=50)
+    State = models.CharField(max_length=2)
+    Zip = models.IntegerField()
+    email = models.CharField(max_length=100)
+    PhoneNum = models.IntegerField()
+
+    def __str__(self):
+        return self.LastName
+
+#supplier table
+class ItemSupplier(models.Model):
+    SupplierName = models.CharField(max_length=100)
+    ValueSuppliedToDate = models.FloatField()
+    FirstItemDate = models.DateField()
+    LastItemDate = models.DateField()
+
+    def __str__(self):
+        return self.SupplierName
+
+#product table
+class Items(models.Model):
+    ItemSupplierID = models.ForeignKey(ItemSupplier, on_delete=models.CASCADE)
+    ItemName = models.CharField(max_length=50)
+    ItemDiscription = models.TextField()
+    ItemMarkup = models.FloatField()
+    SalePrice = models.FloatField()
+    PriceToOrder = models.FloatField()
+    InStock = models.BooleanField()
+
+    def __str__(self):
+        return self.ItemName
+
+#transactions table, connects employee, customer, and items
+class Transaction(models.Model):
+    EmployeeID = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    ItemID = models.ForeignKey(Items, on_delete=models.CASCADE)
+    CustomerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    TransDateTime = models.DateTimeField
+    TransTotal = models.FloatField()
+    BuyOnline = models.BooleanField()
+    buyOffline = models.BooleanField()
+
+    def __str__(self):
+        return self.CustomerID
+
+
+    
